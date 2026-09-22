0fe9ebe1e149c3351f2901b64ea8171d5f986929
unknown <pathakangshuman700@gmail.com>
2026-09-22 09:52:28 +0530

    Preparing to Deploy

--- src/lib/apis/apis.ts
+++ src/lib/apis/apis.ts
@@ -121,7 +121,7 @@ export const deleteBooking = async (bookingData: { vehicleId: string; bookedFrom
 
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.BASE_URL,
   withCredentials: true,
 
 });
