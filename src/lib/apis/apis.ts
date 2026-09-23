994cca25fa09f387e5645a794a7f40fc53c9a225
unknown <pathakangshuman700@gmail.com>
2026-09-23 16:32:01 +0530

    Reviews added 3

--- src/lib/apis/apis.ts
+++ src/lib/apis/apis.ts
@@ -4,7 +4,7 @@ import data from './vehicleData.json'
 import axios from "axios";
 
 
-const VITE_API_BASE_URL = "";
+const VITE_API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 console.log(VITE_API_BASE_URL);
 
